dace5e32a4f41ed1f248a37f06f950b48954bca1
yangxiaoqi@lxlogistics.com <yangxiaoqi@lxlogistics.com>
2019-11-13 11:03:55 +0800

    vue-cli基础语言学习（node.js，webkit打包，vue-cli）

---
+++ todolist/src/components/todoitem.vue
@@ -0,0 +1,22 @@
+
+<template>
+  
+  <li @click="change">{{content}}</li>
+  
+  
+  
+</template>
+  
+<script>
+export default {
+  props:['content','index'],
+  methods:{
+    change:function(){
+    this.$emit('delete',this.index)
+    }
+  }
+}
+</script>
+<style>
+
+</style>
